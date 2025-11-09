9219fb500b299e037e9dcefe29b48dcc5f6f15b6
dudu-cmd11 <duduntj22@gmail.com>
2025-11-09 11:16:43 -0300

    Refactor card variables and input handling

--- Super_Trunfo.c
+++ Super_Trunfo.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    int carta, pt1, pt2;
+
+    /*****Todas variáveis declaradas estão a baixo******/
+
+    int carta1, carta2, pt1, pt2;
     unsigned long int populacao1, populacao2; 
-    char cidade[30];
+    char cidade1[30];
+    char cidade2[30];
     char estado[30];
     char codigo[30];
     float densidade, inverso_densidade, pib_per_capita, superpoder1, superpoder2, area1, area2, pib1, pib2;
 
+    /*****Aqui começa a interação com o jogador*****/
+    /*****E também os dados da Primeira carta*****/
     
     printf("**Bem vindo ao nosso jogo SuperTrunfo**\n\n");
     printf("Preencha os campos da primeira carta:\n\n");
 
     printf("Qual o número da sua carta:\n");
-    scanf("%d", &carta);
+    scanf("%d", &carta1);
 
     printf("Qual o codigo de sua carta:\n");
     scanf("%s", codigo);
@@ -22,7 +28,7 @@ int main() {
     scanf(" %[^\n]", estado);
 
     printf("Nome da cidade:\n");
-    scanf(" %[^\n]", cidade);
+    scanf(" %[^\n]", cidade1);
 
     printf("População: \n");
     scanf("%lu", &populacao1);
@@ -36,18 +42,20 @@ int main() {
     printf("Quantos pontos turisticos tem em sua cidade: \n");
     scanf("%d", &pt1);
 
+    /*****Aqui inicia o calculo de alguns atributos da primeira carta*****/
     
     densidade = (float)populacao1 / area1;
     inverso_densidade = 1.0 / densidade;
     pib_per_capita = pib1 / (float)populacao1;
     superpoder1 = (float)populacao1 + area1 + pib1 + inverso_densidade + pib_per_capita + pt1;
 
+    /***** Já aqui seria a parte em que as informações colocadas pelo jogador seja exibida em tela *****/
     
     printf("\n *Dados da carta* 1 \n");
-    printf("Número da carta: %d\n", carta);
+    printf("Número da carta: %d\n", carta1);
     printf("Estado: %s\n", estado);
     printf("Codigo da carta: %s\n", codigo);
-    printf("Cidade: %s\n", cidade);
+    printf("Cidade: %s\n", cidade1);
     printf("População: %lu\n", populacao1);
     printf("Área: %.2f\n", area1);
     printf("PIB: %.2f\n", pib1);
@@ -58,11 +66,12 @@ int main() {
     printf("Inverso da densidade: %.2f\n", inverso_densidade);
     printf("Super poder da carta: %.2f\n\n", superpoder1);
 
+    /*****  Aqui inicia a coleta de dados da segunda carta *****/
    
     printf("Agora iremos para a segunda carta!\n\n");
 
     printf("Qual o número da sua carta:\n");
-    scanf("%d", &carta);
+    scanf("%d", &carta2);
 
     printf("Qual o codigo de sua carta:\n");
     scanf("%s", codigo);
@@ -71,7 +80,7 @@ int main() {
     scanf(" %[^\n]", estado);
 
     printf("Nome da cidade:\n");
-    scanf(" %[^\n]", cidade);
+    scanf(" %[^\n]", cidade2);
 
     printf("População: \n");
     scanf("%lu", &populacao2);
@@ -85,18 +94,20 @@ int main() {
     printf("Quantos pontos turisticos tem em sua cidade: \n");
     scanf("%d", &pt2);
 
+    /*****Aqui inicia o calculo de alguns atributos da segunda carta*****/
     
     densidade = (float)populacao2 / area2;
     inverso_densidade = 1.0 / densidade;
     pib_per_capita = pib1 / (float)populacao2;
     superpoder2 = (float)populacao2 + area2 + pib1 + inverso_densidade + pib_per_capita + pt2;
 
+    /***** Já aqui seria a parte em que as informações colocadas pelo jogador seja exibida em tela *****/
     
     printf("\n *Dados da carta 2* \n");
-    printf("Número da carta: %d\n", carta);
+    printf("Número da carta: %d\n", carta2);
     printf("Estado: %s\n", estado);
     printf("Codigo da carta: %s\n", codigo);
-    printf("Cidade: %s\n", cidade);
+    printf("Cidade: %s\n", cidade2);
     printf("População: %lu\n", populacao2);
     printf("Área: %.2f\n", area2);
     printf("PIB: %.2f\n", pib2);
@@ -107,7 +118,20 @@ int main() {
     printf("Inverso da densidade: %.2f\n", inverso_densidade);
     printf("Super poder da carta: %.2f\n\n", superpoder2);
 
+    printf("Agora iremos saber qual das cartas tem a maior população.\n");
+    printf("%d - %s: %lu\n", carta1, cidade1, populacao1);
+    printf("%d - %s: %lu\n", carta2, cidade2, populacao2);
+    if (populacao1 > populacao2)
+    {
+    printf("A %d venceu - %s: %lu\n", carta1, cidade1, populacao1);
+    }
+    else
+    {
+    printf("A %d venceu - %s: %lu\n", carta2, cidade2, populacao2);
+    }
     
+
+    /*
     printf("Agora vamos para a comparação das cartas!\n");
 
     printf("A população da carta 1 é maior que o da carta 2? %d\n", (populacao1 > populacao2));
@@ -115,5 +139,7 @@ int main() {
     printf("O PIB da carta 1 é maior que o da carta 2? %d\n", (pib1 > pib2));
     printf("Os pontos turísticos da carta 1 são maiores que o da carta 2? %d\n", (pt1 > pt2));
     printf("O SuperPoder da carta 1 é maior que o da carta 2? %d\n", (superpoder1 > superpoder2));
+    */
+
     return 0;
 }
