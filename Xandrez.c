89592d8fa73619d11869913880fde93e1185b0f5
dudu-cmd11 <duduntj22@gmail.com>
2025-12-02 05:40:48 -0300

    Implement knight movement logic in Xandrez.c
    Added logic for moving the knight piece in the game.

--- Xandrez.c
+++ Xandrez.c
@@ -6,7 +6,7 @@ int main(){
     // Bispo se move somente 5 casas para a diagonal direita Ex: Cima, Direita
     // Rainha se move 8 casas para a esquerda
 
-    int peca;
+    int peca, cavalo, i;
 
     printf("1 - Torre\n2 - Bispo\n3 - Rainha\n4 - Cavalo\n");
 
@@ -43,7 +43,14 @@ int main(){
     }
     if (peca == 4)
     {
-    for (int j = 1; j <= 2; j++)
+        printf("Para qual lado deseja movimentar o cavalo:\n");
+        printf("1 - Direita\n2 - Esquerda\n");
+        scanf("%d", &cavalo);
+
+        switch (cavalo)
+        {
+        case 1:
+                for (int j = 1; j <= 2; j++)
     {
             printf("Cima\n");
     }
@@ -51,13 +58,27 @@ int main(){
         while (i <= 1)
         {
             printf("Direita\n");
-            i++;
-            
+            i++; 
         }
-        
-        
+            break;
+        case 2:
+            for (int j = 1; j <= 2; j++)
+    {
+            printf("Cima\n");
     }
-    
+        i = 1;
+        while (i <= 1)
+        {
+            printf("Esquerda\n");
+            i++; 
+        }
+        break;
+
+        default:
+
+            break;
+    }
+}
     
     return 0;
 }
